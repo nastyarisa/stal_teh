a3fb51c0e7f5426192ee0c478b5c2d14b299da00
ana_smir <pl_nastya@mail.ru>
2019-05-30 19:50:28 +0300

    update to gulp 4, change about us

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,127 @@
 //npm i --save-dev gulp gulp-less gulp-plumber gulp-postcss gulp-posthtml posthtml-include autoprefixer browser-sync
 //npm i --save-dev gulp-csso gulp-rename gulp-imagemin gulp-webp run-sequence del gulp-uglify pump gulp-pug
 
-var gulp = require("gulp");
-var babel = require('gulp-babel');
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var minify = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var run = require("run-sequence");
-var del = require("del");
+const {gulp, src, dest, parallel, series, watch } = require('gulp');
+// const gulp = require("gulp");
+// const babel = require('gulp-babel');
+const less = require("gulp-less");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const minify = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const run = require("run-sequence");
+const del = require("del");
 const uglify = require('gulp-uglifyes');
+const pug = require('gulp-pug');
 // var uglifyes = require('uglify-es');
 // var composer = require("gulp-uglify/composer");
 // var pump = require('pump');
 
 // var minify = composer(uglifyes, console);
 
-var pug = require('gulp-pug');
-
-gulp.task('pug', function buildHTML() {
-  return gulp.src('pug/**/*.pug')
+function html() {
+  return src('pug/**/*.pug')
   .pipe(pug({
     pretty:true
   }))
-  .pipe(gulp.dest("build"));
-})
+  .pipe(dest("build"));
+}
 
-gulp.task("less", function() {
-  gulp.src("less/style.less")
+function css() {
+  return src("less/style.less")
   .pipe(plumber())
   .pipe(less())
   .pipe(postcss([
     autoprefixer()
   ]))
-  .pipe(gulp.dest("css"))
-  .pipe(gulp.dest("build/css"))
+  .pipe(dest("css"))
+  .pipe(dest("build/css"))
   .pipe(minify())
   .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("build/css"))
-  .pipe(server.stream());
-});
+  .pipe(dest("build/css"))
+  .pipe(browsersync.stream());
+};
 
-gulp.task("images", function() {
-  return gulp.src("img/**/*.{png,jpg,svg}")
+function images() {
+  return src("img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("img"));
-});
+    .pipe(dest("img"));
+};
 
-gulp.task("webp", function() {
-  return gulp.src("img/**/*.{png,jpg}")
+function webP() {
+  return src("img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"));
-});
+    .pipe(dest("img"));
+};
 
-function es(){
-  return gulp.src('js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
+function compress() {
+  return src('js/**/*.js')
+  .pipe(uglify())
+  .pipe(dest('build/js'));
 }
- 
-gulp.task('compress', es);
 
-gulp.task("serve", function() {
-  server.init({
-    server: "build/"
+// function browserSync(done) {
+//   browsersync.init({
+//     server: "build/",
+//     port: 3000
+//   });
+//   done();
+// }
+
+// BrowserSync Reload
+// function browserSyncReload(done) {
+//   browsersync.reload();
+//   done();
+// }
+
+function serve() {
+  // server.init({
+  //   server: "build/"
+  // });
+  browsersync.init({
+    baseDir: "./",
+    server: "build/",
+    port: 3000
   });
-  gulp.watch("less/**/*.less", ["less"]);
-  gulp.watch("pug/**/*.pug", ["pug"]);
-  gulp.watch("js/**/*.js", ["compress"]);
-});
+  watch("less/**/*.less", css);
+  watch("pug/**/*.pug").on('change', series(html, browsersync.reload));
+  watch("js/**/*.js").on('change', series(compress, browsersync.reload));
+};
 
-gulp.task("copy", function() {
-  return gulp.src([
+
+function copy() {
+  return src([
     "fonts/**/*.{ttf}",
     "img/**"
   ], {
     base: "."
   })
-  .pipe(gulp.dest("build"));
-});
+  .pipe(dest("build"));
+};
 
-gulp.task("clean", function() {
+
+function clean() {
   return del("build");
-});
+};
 
 
-gulp.task("build", function(done) {
-  run(
-    "clean",
-    "pug",
-    "copy",
-    "less",
-    "compress",
-    done
-  );
-});
+// const serve = parallel(watchFiles, browserSync);
+const build = series(clean, html, copy, css, compress);
+
+exports.clean = clean;
+exports.copy = copy;
+exports.compress = compress;
+exports.webP = webP;
+exports.images = images;
+exports.css = css;
+exports.html = html;
+exports.serve = serve;
+exports.default = build;
